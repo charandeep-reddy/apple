d93446879339afeda95a50bfe2c28512c26b7d95
Gnaneswar Sai <gnaneswarsai117@gmail.com>
2025-05-02 16:08:48 +0530

    Fix Carousel width

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,7 +2,7 @@ function Carousel({ children }) {
   return (
     <div className="w-full overflow-x-auto scroll-smooth max-lg:snap-none scrollbar-hide snap-x py-4 antialiased">
       <div className="flex gap-5 justify-start w-max">
-        <div className="w-[calc(18vw-155px)] snap-end" aria-hidden="true"></div>
+        <div className="w-[calc(18vw-160px)] snap-end" aria-hidden="true"></div>
         {children}
         <div className="w-[50px] snap-end" aria-hidden="true"></div>
       </div>
